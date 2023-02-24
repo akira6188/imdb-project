d7a50ebb87585c564697d0a4758c003901fed466
akira6188 <daigocafe22@gmail.com>
2023-02-24 21:40:20 +0900

    create the header section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,5 +4,5 @@ import { Inter } from '@next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1 className="bg-gray-600">Hello</h1>;
+  return <h1 className="text-red-200">Home</h1>;
 }
