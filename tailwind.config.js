41b8a5369d46fe6d85ec06bc547cb94008ca2c08
akira6188 <daigocafe22@gmail.com>
2023-02-24 20:36:01 +0900

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // If using `public` directory
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
